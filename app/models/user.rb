7dd9e849451d2f6dbda6affbb586ae3a86237b83
randomplayereg <randomplayer.eg@gmail.com>
2018-03-19 15:08:45 +0700

    link-up user - book

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  has_many :books
   validates :username, presence: true, uniqueness: true, length: {minimum: 5, maximum: 25}
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+  before_save {self.email = email.downcase}
 end
