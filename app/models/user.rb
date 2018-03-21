3e60ab5e3217857df5b8e58155195f454b7164b1
randomplayereg <randomplayer.eg@gmail.com>
2018-03-21 14:35:52 +0700

    refactoring + some minor tweak

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   validates :username, presence: true, uniqueness: true, length: {minimum: 5, maximum: 25}
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: true, format: {with: VALID_EMAIL_REGEX}  
   has_secure_password
 
   before_save {self.email = email.downcase}
